05776a0c9ace5872cc28693f15120b9b0a026c4f
Scott Whitman <scottnj@gmail.com>
2020-08-21 10:18:12 -0400

    setup gradle, DI, main view model

--- app/src/main/java/com/example/paging3example/MainActivity.kt
+++ app/src/main/java/com/example/paging3example/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.paging3example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvMessage.text = viewModel.message
     }
 }
---
+++ app/src/main/java/com/example/paging3example/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.paging3example
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val message = "Hello From ViewModel"
+}
